c05468a3b0402fa683378ea2c594597c7c31b157
Hrushikesh17 <147137274+Hrushikesh17@users.noreply.github.com>
2024-11-22 11:17:02 +0530

    Add files via upload

---
+++ freetre/src/main/java/com/hkp/freetre/FreetreApplication.java
@@ -0,0 +1,14 @@
+package com.hkp.freetre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreetreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreetreApplication.class, args);
+		System.out.println("FreeTree just Started...");
+	}
+
+}
